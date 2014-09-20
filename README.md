3118c16c8aa85215d5e9546898355a613b90a1e1
Rufus Pollock <rufus.pollock@okfn.org>
2014-09-20 12:07:58 +0100

    [all][l]: major update both to scripting and to data.
    scripts: handle constantly changing source data urls and different source data format.
    
    * new find_links method to find the download links on S&P website (they change
      every release afaict)
    * cache all the raw source data into archive
    * refactor extraction as source data has changed. source data files have
      changed and national data now provided monthly along with cities in one
      single xls (archive/home-price-index-levels.xls). As such just have one output file
    
    data: consolidate data in one file and get latest data
    
    * remove separate national data files (see previous item in scripts)
    * cities-month now has national data in last column plus last year of data
      (plus some changes to old data - weird!) - git diff --word-diff --color-words
      is your friend!
    
    datapackage.json: as per data
    
    README.md: remove obsolete data links and add some excerpted info on methodology etc
    
    archive: add the raw xls for home-price-index-levels

--- README.md
+++ README.md
@@ -4,23 +4,49 @@ metropolitan regions. The indices are created using a repeat-sales methodology.
 
 ## Data
 
-There is [home page for Indices on S&P website][sp-home]. This provides a table
-of links but these are not direct file URLs and you have dig around in S&P's
-javascript to find the actual download locations:
+As per the [home page for Indices on S&P website][sp-home]:
+
+> The S&P/Case-Shiller U.S. National Home Price Index is a composite of
+> single-family home price indices for the nine U.S. Census divisions and is
+> calculated monthly. It is included in the S&P/Case-Shiller Home Price Index
+> Series which seeks to measure changes in the total value of all existing
+> single-family housing stock.
+
+Documentation of the methodology can be found at:
+<http://www.spindices.com/documents/methodologies/methodology-sp-cs-home-price-indices.pdf>
+
+Key points are (excerpted from methodology):
+
+* The indices use the "repeat sales method" of index calculation which uses
+  data on properties that have sold at least twice, in order to capture the
+  true appreciated value of each specific sales unit.
+* The quarterly S&P/Case-Shiller U.S. National Home Price Index aggregates nine
+  quarterly U.S. Census division repeat sales indices using a base period a nd
+  estimates of the aggregate value of single family housing stock for those periods.
+* The S&P/Case - Shiller Home Price Indices originated in the 1980s by Case
+  Shiller Weiss's research principals, Karl E. Case and Robert J. Shiller. At
+  the time, Case and Shiller developed the repeat sales pricing technique. This
+  methodology is recognized as the most reliable means to measure housing price
+  movements and is used by other home price ind ex publishers, including the
+  Office of Federal Housing Enterprise Oversight (OFHEO)
 
 [sp-home]: http://www.spindices.com/index-family/real-estate/sp-case-shiller
 
-* [National index][nat] (xls)
-* [City indices][city] (xls)
-
-[nat]: http://us.spindices.com/documents/additionalinfo/20131126/64929_csnational-values-1126.xls'
-[city]: http://us.spindices.com/documents/additionalinfo/20131126/64929_cshomeprice-history-1126.xls
-
-## Wrangling
+## Preparation
 
 To download and process the data do:
 
     python scripts/process.py
 
 Updated data files will then be in `data` directory.
+
+Note: the URLs and structure of the source data have evolved over time with the
+source data URLs changing on *every release*.
+
+Originally (2013) the site provided a table of links but these are not direct
+file URLs and you have dig around in S&P's javascript to find the actual
+download locations. As of mid-2014 the data is consolidated in one primary XLS
+but the HTML you see in your browser and the source HTML are different. In
+addition, the actual location of the XLS file continues to change on each
+release.
 
--- scripts/process.py
+++ scripts/process.py
@@ -2,77 +2,97 @@ import os
 import csv
 import urllib
 import shutil
+import logging
+import ssl
+from functools import partial
 import dataconverters
 
-url_national = 'http://us.spindices.com/documents/additionalinfo/20131126/64929_csnational-values-1126.xls'
-url_cities = 'http://us.spindices.com/documents/additionalinfo/20131126/64929_cshomeprice-history-1126.xls'
-xls_national = 'tmp/national.xls'
-xls_cities = 'tmp/cities.xls'
-tmp_national = 'tmp/national.csv'
-tmp_cities = 'tmp/cities.csv'
-out_national_year = 'data/national-year.csv'
-out_national_quarter = 'data/national-quarter.csv'
-out_cities = 'data/cities-month.csv'
+logger = logging.getLogger()
 
 def setup():
     if not os.path.exists('tmp'):
         os.makedirs('tmp')
     if not os.path.exists('data'):
         os.makedirs('data')
+    if not os.path.exists('archive'):
+        os.makedirs('archive')
+
+def find_links():
+    '''Find all the download links in the page - they change every month'''
+    url = 'http://www.spindices.com/indices/real-estate/sp-case-shiller-us-national-home-price-index'
+    content = urllib.urlopen(url).read()
+    import re
+    # download links are in dropdown from additional item page now
+    # note in web browser they are "a" links but in curl etc they are <option>
+    pattern = '<option value="([^"]*)">([^<]*)</option>'
+    links = re.findall(pattern, content)
+    return links
 
 def retrieve():
-    urllib.urlretrieve(url_national, xls_national)
-    urllib.urlretrieve(url_cities, xls_cities)
+    '''Retrieve the raw data to the local cache (archive directory)
 
-def xls_to_csv():
-    dataconverters.dataconvert(xls_national, tmp_national)
-    dataconverters.dataconvert(xls_cities, tmp_cities, guess_types=False)
+    OK so we have to mess with ssl ... Why?
 
-def process_national():
-    fo = open(tmp_national)
-    reader = csv.DictReader(fo)
-    foout = open(out_national_year, 'w')
-    writer = csv.writer(foout)
-    writer.writerow(['Year', 'Composite-US'])
-    vals = []
-    for row in reader:
-        year = row['YEAR'][:4]
-        quarter = row['QTR'][1]
-        vals.append(float(row['COMPOSITE-US']))
-        if quarter == '4':
-            val = round(sum(vals) / 4, 2)
-            vals = []
-            writer.writerow([year, val])
+    So, let's walk through some steps
 
-def process_national_q():
-    fo = open(tmp_national)
-    reader = csv.DictReader(fo)
-    foout = open(out_national_quarter, 'w')
-    writer = csv.writer(foout)
-    writer.writerow(['Date', 'Composite-US'])
-    for row in reader:
-        year = row['YEAR'][:4]
-        date = '%s-%02d-01' % (year, (int(row['QTR'][1])-1)*3+1)
-        writer.writerow([date, row['COMPOSITE-US']])
+    1. We try to get a URL like
+       http://www.spindices.com/documents/additionalinfo/20140826-107542/107542_cs-condoindices-0826.xls?force_download=true
 
-def process_cities():
-    indata = open(tmp_cities).read()
+    2. This redirects to an SSL URL
+       (https://www.spice-indices.com/idpfiles/spice-assets/resources/public/documents/107542_cs-condoindices-0826.xls?force_download=true)
+
+    3. If you try downloading this normally the code hangs (and times out)
+
+    4. curl also hangs. With a bit of debugging you see it just hangs after
+       reporting "SSLv3, TLS handshake, Client hello (1)"
+
+    5. Fixed by adding -3 option to curl (forcing use of a specific ssl version
+       AFAICT)
+    
+    6. A bit more use of interwebs suggests the hack below as a way to do this
+       in python (and it works)
+    '''
+    # have monkey patch ssl - see notes in docstring
+    ssl.wrap_socket = partial(ssl.wrap_socket, ssl_version=ssl.PROTOCOL_SSLv3)
+
+    for linkOffset, name in find_links():
+        if linkOffset.endswith('.pdf?force_download=true'):
+            continue
+        url = 'http://www.spindices.com' + linkOffset
+        fn = name.strip().lower().replace(' ', '-') + '.xls'
+        dest = os.path.join('archive', fn)
+        logger.info('Retrieving: %s' % url)
+        urllib.urlretrieve(url, dest)
+        logger.info('Saved to: %s' % dest)
+
+
+def extract():
+    '''Extract data from cached raw data files in archive and write to data/
+    '''
+    source = 'archive/home-price-index-levels.xls'
+    # all-month.csv might be more appropriate but we wanted to keep continuity
+    # with cities-month.csv (before mid 2014 city data and national data were
+    # provided separately but now there is just one file with everything) 
+    out_path = 'data/cities-month.csv'
+
+    tmp_out = os.path.join('tmp', 'home-price-index-levels.csv')
+    dataconverters.dataconvert(source, tmp_out, guess_types=False)
+
+    indata = open(tmp_out).read()
     # fix time in dataconvert which adds 00:00:00
     indata = indata.replace(' 00:00:00', '')
     indata = indata.replace('column_1', 'Date')
     indata = indata.split('\n')
     del indata[1]
     indata = '\n'.join(indata)
-    open(out_cities, 'w').write(indata)
+    open(out_path, 'w').write(indata)
 
 def process():
     setup()
     retrieve()
-    xls_to_csv()
-    process_national()
-    process_national_q()
-    process_cities()
+    extract()
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
     process()
 
